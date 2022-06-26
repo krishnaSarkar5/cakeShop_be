102109bc1cce3d59739b104a4ed437b5621d259d
krishnaSarkar5 <krishnasarkar95@gmail.com>
2022-06-26 14:27:39 +0530

    some changes

--- src/main/java/com/cakeShop/controller/DemoController.java
+++ src/main/java/com/cakeShop/controller/DemoController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 
 	@GetMapping
-	public void demo() {
-		
+	public String demo() {
+		return "Hello!!";
 	}
 }
